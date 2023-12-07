da34071193e08d8a5472520a72faa660bae04ef2
alyssabmcnatt <6654@holbertonstudents.com>
2023-12-06 21:30:55 -0800

    idc

---
+++ string.c
@@ -0,0 +1,79 @@
+#include "shell.h"
+
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+char *starts_with(const char *haystack, const char *needle);
+char *_strcat(char *dest, char *src);
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ * Return: intefer length of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * _strcmp - performs lexicographic comparison of two strings
+ * @s1: the first string
+ * @s2: the second string
+ * Return: -1 if s1 < s2, 1 if s1 > s2, 0 if s1 == s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
+/**
+ * start_with - checks if needles starts with haystack
+ * @haystack: string to search
+ * @needle: the substring to find
+ * Return: address of next char of haystack or NULL
+ */
+
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: the destination buffer
+ * @src: the source buffer
+ * Return: pointer to destination buffer
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
+}
